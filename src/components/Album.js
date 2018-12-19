efa5d28dbe93b5ff98a511e0bae02bb52d2006ac
Judy Huynh <jhuynh@mossbeachhomes.com>
2018-12-18 16:20:14 -0800

    Audio Playback: added play button..thats it.

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,9 +10,49 @@
      });
 
      this.state= {
-       album:album
+       album:album,
+       currentSong: album.songs[0],
+       isPLaying: false,
+       isMouseInside: false
      };
+     this.audioElement = document.createElement('audio');
+     this.audioElement.src = album.songs[0].audioSrc;
    }
+
+play(){
+  this.audioElement.play();
+  this.setState({ isPlaying: true });
+}
+
+pause(){
+  this.audioElement.pause();
+  this.setState({ isPlaying: false});
+}
+
+setSong(song){
+  this.audioElement.src = song.audioSrc;
+  this.setState({ currentSong: song });
+}
+
+handleSongClick(song) {
+  const isSameSong = this.state.currentSong === song;
+  if (this.state.isPlaying && isSameSong) {
+    this.pause();
+  } else {
+    if (!isSameSong) { this.setSong(song); }
+    this.play();
+  }
+}
+
+
+mouseEnter(){
+  this.setState({ isMouseInside: true });
+}
+mouseLeave(){
+
+  this.setState({ isMouseInside: false });
+}
+
 
   render() {
     return (
@@ -25,13 +65,15 @@
           <div id="release-info">{this.state.album.releaseInfo}</div>
         </div>
       </section>
-      <table id="song-list">
+      <table id="song-list" align="center">
         <colgroup>
           <col id="song-number-column" />
           <col id="song-title-column" />
           <col id="song-duration-column" />
         </colgroup>
       <tbody>
+          <tr>
+            <th colspan= "3"> SONGS: </th></tr>
           <tr>
             <th>Num</th>
             <th>Title</th>
@@ -40,12 +82,17 @@
 
             {
 
-            this.state.album.songs.map((currentvalue, index)=>
+            this.state.album.songs.map((song, index)=>
 
-            <tr key={index}>
-              <td> {index + 1} </td>
-              <td> {currentvalue.title}</td>
-              <td> {currentvalue.duration}</td>
+            <tr className="song" key={index} onClick={() => this.handleSongClick(song)}  onMouseEnter={() => this.mouseEnter()} onMouseLeave={() => this.mouseLeave()}>
+
+              <td className= "song-number">
+            <button className="ion-play"></button> {index}
+
+
+              </td>
+              <td className= "song-title"> {song.title}</td>
+              <td className= "song-duration"> {song.duration}</td>
             </tr>
               )
             }
