c4f81a3960c91cf8e264b6977981ecb494186568
judy <judy@Users-MBP-4.attlocal.net>
2019-01-01 16:33:20 -0800

    completed styling

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import albumData from './../data/albums';
+import './Library.css';
 
 class Library extends Component {
   constructor(props) {
@@ -11,18 +12,33 @@ class Library extends Component {
 
   render() {
      return (
+
        <section className="library" id="library">
+
          {
            this.state.albums.map( (album, index) =>
-             <Link to={`/album/${album.slug}`} key={index}>
-               <img src={album.albumCover} alt={album.title} />
-               <div>{album.title}</div>
-               <div>{album.artist}</div>
-               <div>{album.songs.length} songs</div>
-             </Link>
+
+              <div className="card flex-row flex-wr" style={{width: '12rem', height: '12rem'}}>
+
+                 <img className="card-img-top" src={album.albumCover} alt={album.title} />
+                   <div className="card-body">
+                     <div style={{width: '18rem'}}>
+                       <h4 class="card-title">{album.title}</h4>
+                     </div>
+                     <div className="card-text">
+                       {album.artist} | {album.songs.length} songs
+                     </div>
+                     <Link to={`/album/${album.slug}`} key={index}>
+                       <div className="btn btn-primary" style={{ marginTop: '1rem' }}>
+                         View Album
+                       </div>
+                    </Link>
+                  </div>
+               </div>
            )
          }
        </section>
+
     );
   }
 }
--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -4,42 +4,49 @@ import React, { Component } from 'react';
    render() {
      return (
        <section className="player-bar">
-       <section id="buttons">
-        <button id="previous" onClick={this.props.handlePrevClick}>
-          <span className="ion-md-skip-backward"></span>
-        </button>
-        <button id="play-pause" onClick={this.props.handleSongClick} >
-        <span className={this.props.isPlaying ? 'ion-md-pause' : 'ion-md-play'}></span>
-        </button>
-        <button id="next" onClick={this.props.handleNextClick}>
-          <span className="ion-md-skip-forward"></span>
-        </button>
+         <section id="buttons" style={{display: 'flex', justifyContent: 'center'}}>
+          <button id="previous" onClick={this.props.handlePrevClick}>
+            <span className="ion-md-skip-backward" style={{ fontSize: '30px'}}></span>
+          </button>
+          <button id="play-pause" onClick={this.props.handleSongClick} >
+            <span className={this.props.isPlaying ? 'ion-md-pause' : 'ion-md-play'} style={{ fontSize: '30px', color:'#8da38c'}}></span>
+          </button>
+          <button id="next" onClick={this.props.handleNextClick}>
+            <span className="ion-md-skip-forward" style={{ fontSize: '30px'}}></span>
+          </button>
+        </section>
+      <section id="time-control" style={{display: 'flex', justifyContent: 'center'}}>
+
+        {this.props.formatTime(this.props.currentTime)}
+
+          <input
+            type="range"
+            className="seek-bar"
+            value={(this.props.currentTime / this.props.duration) || 0}
+            max="1"
+            min="0"
+            step="0.01"
+            onChange={this.props.handleTimeChange}
+          />
+
+            {this.props.formatTime(this.props.duration)}
+
       </section>
-      <section id="time-control">
-      <div className="current-time">{this.props.formatTime(this.props.currentTime)}</div>
-      <input
-        type="range"
-        className="seek-bar"
-        value={(this.props.currentTime / this.props.duration) || 0}
-        max="1"
-        min="0"
-        step="0.01"
-        onChange={this.props.handleTimeChange}
-      />
-      <div className="total-time">{this.props.formatTime(this.props.duration)}</div>
-      </section>
-      <section id="volume-control">
-      <div className="volume" />
-      <input
-        type="range"
-        className="seek-bar"
-        value={this.props.volume}
-        max="1"
-        min="0"
-        step="0.01"
-        onChange={this.props.handleVolumeChange}
-      />
-      </section>
+
+          <section id="volume-control" style={{display: 'flex', justifyContent: 'center'}}>
+            <div className="volume" />
+              <ion-icon name="volume-mute"></ion-icon>
+            <input
+              type="range"
+              className="seek-bar"
+              value={this.props.volume}
+              max="1"
+              min="0"
+              step="0.01"
+              onChange={this.props.handleVolumeChange}
+            />
+          <ion-icon name="volume-high"></ion-icon>
+          </section>
        </section>
      );
    }
